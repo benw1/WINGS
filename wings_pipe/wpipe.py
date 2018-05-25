609687db65327a8d43bd43cff24ffbd14289a4c2
Rubab Khan <rubab@uw.edu>
2018-05-25 13:32:34 -0700

    Update wpipe.py

--- wings_pipe/wpipe.py
+++ wings_pipe/wpipe.py
@@ -6,11 +6,11 @@ from astropy import wcs
 from astropy.io import fits, ascii
 from astropy.table import Table
 
-def _to_dict(x):
+def to_dict(x):
     '''x is any class instance'''
     return x.__dict__
 
-def _to_json(x):
+def to_json(x):
     '''x is any class instance'''
     return json.dumps(x.__dict__)
 
@@ -21,10 +21,10 @@ def _update(x,y):
     else:
         x.timestamp = time.time()
     return x
-
-def _increment_option(**opt):
-    
-
+'''
+def increment(x):
+    return int(x)+1
+'''
 
 class User():
     def __init__(self,name='any',user_id=1):
@@ -32,6 +32,14 @@ class User():
         self.user_id = int(user_id)
         return None
 
+class Node():
+    def __init__(self,name='any',node_id=1,int_ip='',ext_ip=''):
+        self.name = str(name)
+        self.node_id = int(node_id)
+        self.int_ip = int_ip
+        self.ext_ip = ext_ip
+        return None
+    
 class Options():
     def __init__(self,opts):
         if not isinstance(opts,dict):
@@ -40,11 +48,11 @@ class Options():
         self.timestamp = time.time()
         return None
 
-    @clsmethod
+    #@clsmethod
     def update(cls,opts):
         return _update(cls,opts)
         
-    @staticmethod
+    #@staticmethod
     def myOptions(options):
         if isinstance(options,Options):
             return options
@@ -55,7 +63,7 @@ class Options():
 
         
 class Pipeline(User,Options):
-    def __init__(self,name='',pipeline_id=1,software_root='',
+    def __init__(self,user='',name='',pipeline_id=1,software_root='',
                  data_root='',pipe_root='',configuration_root='',
                  description='',state_id=0,options=''):
         if not isinstance(user,User): raise AssertionError('Did not get User instance')
@@ -73,7 +81,7 @@ class Pipeline(User,Options):
         self.state_id = state_id
         self.timestamp = time.time()
         return None
-
+'''
     def update(self):
 
 
@@ -82,7 +90,7 @@ class Pipeline(User,Options):
 
     def delete(self):
         
-    
+'''
 class Target(Pipeline):
     def __init__(self,name='',target_id=1,relativepath='',pipeline='',options=''):
         if not isinstance(name,str):
@@ -110,53 +118,51 @@ class Configuration(Target):
         self.pipeline_id = target.pipeline_id
         self.config_id = int(config_id)
         self.description = description
-        self.options = pipeline.options
+        self.options = target.options
         if isinstance(options,dict): self.options.update(options)
         return None
         
 class DataProduct(Configuration):
-    def __init__(self,filename,relativepath,group,configuration,
-                 data_type='',subtype='',suffix='',filtername='',
+    def __init__(self,filename='',relativepath='',group='',configuration='',
+                 dp_id=1,data_type='',subtype='',suffix='',filtername='',
                  ra=0,dec=0,pointing_angle=0,options=''):
-    if not isinstance(configuration,Configuration):
-        raise AssertionError('Did not get Configurations instance')
+        if not isinstance(configuration,Configuration):
+            raise AssertionError('Did not get Configurations instance')
 
-    self.configuration = configuration
-    self.config_id = configuration.config_id
-    self.target_id = configuration.target_id
-    self.pipeline_id = configuration.pipeline_id
-    self.dp_id = dp_id
+        self.configuration = configuration
+        self.config_id = configuration.config_id
+        self.target_id = configuration.target_id
+        self.pipeline_id = configuration.pipeline_id
+        self.dp_id = dp_id
 
-    self.filename = str(filename)
-    self.relativepath = str(relativepath)
-    
-    if '.' in filename:
-        _suffix = filename.split('.')[-1]
-        if _suffix in ['fits','txt','head','cl','py','pyc','pl',
-                'phot','png','jpg','ps','gz','dat','lst','sh']:
-            self.suffix = _suffix
+        self.filename = str(filename)
+        self.relativepath = str(relativepath)
+
+        if '.' in filename:
+            _suffix = filename.split('.')[-1]
+            if _suffix in ['fits','txt','head','cl','py','pyc','pl',
+                    'phot','png','jpg','ps','gz','dat','lst','sh']:
+                self.suffix = _suffix
+            else:
+                self.suffix = 'other'
+
+        if not(data_type): self.data_type = str(self.suffix)
+        self.subtype = str(subtype)
+
+        if group in ['proc','conf','log','raw']:
+            self.group = group
         else:
-            self.suffix = 'other'
+            self.group = str('other')
 
-    if not(data_type): self.data_type = str(self.suffix)
-    self.subtype = str(subtype)
-    
-    if group in ['proc','conf','log','raw']:
-        self.group = group
-    else:
-        self.group = str('other')
-    
-    self.filtername = str(filtername)
-    self.ra = float(ra)
-    self.dec = float(dec)
-    self.pointing_angle = float(pointing_angle)
+        self.filtername = str(filtername)
+        self.ra = float(ra)
+        self.dec = float(dec)
+        self.pointing_angle = float(pointing_angle)
 
-    self.options = configuration.options
-    if isinstance(options,dict): self.options.update(options)
-    
-    self.timestamp = time.time()
-    return None
-
+        self.options = configuration.options
+        if isinstance(options,dict): self.options.update(options)
+        self.timestamp = time.time()
+        return None
 
 class CopyState(DataProduct,Node):
     def __init__(self,dp='',node='',state='any',state_id=0):
@@ -176,7 +182,7 @@ class CopyState(DataProduct,Node):
     
 class Parameters(Configuration):
     def __init__(self,config='',params='',volatile=0):
-        if not isinstance(configuration,Configuration):
+        if not isinstance(config,Configuration):
             raise AssertionError('Did not get Configurations instance')
         if not isinstance(params,dict):
             raise AssertionError('Did not get Parameters dictionary')
@@ -188,7 +194,7 @@ class Parameters(Configuration):
         self.timestamp = time.time()
         return None
 
-    @clsmethod
+    #@clsmethod
     def update(cls,params):
         return _update(cls,params)
 
@@ -196,7 +202,7 @@ class Parameters(Configuration):
 class Task(Pipeline):
     def __init__(self,name='',task_id=1,flags='',
                  pipeline='',nruns=1,run_time=0,
-                 is_exclusive=0,options=''):
+                 is_exclusive=0,mask=[],options=''):
         if not isinstance(name,str):
             raise AssertionError('Did not get target name as string')
         if not isinstance(pipeline,Pipeline):
@@ -207,26 +213,27 @@ class Task(Pipeline):
         self.task_id = int(task_id)
         self.options = pipeline.options
         if isinstance(options,dict): self.options.update(options)
+        self.mask = mask
         self.timestamp = time.time()
-
+        return None
+    
+    def addMask(self,mask=[]):
+        if not isinstance(mask,Mask): raise AssertionError('Did not get Mask instance')
+        self.mask.append(mask)
 
         
-class Mask(Task):
-    def __init__(self,task='',source='',name='',value=''):
-        if not isinstance(task,Task):
-            raise AssertionError('Did not get Task instance')
-        if (not(source)|not(name)|not(value)):
-            raise AssertionError('Did not get source, name and value')
+class Mask():
+    def __init__(self,source='',name='',value=''):
+        if not(source): raise AssertionError('Did not get source')
+        if not(name): raise AssertionError('Did not get name')
+        if not(value): raise AssertionError('Did not get value')
         # 'source' has to be an existing task_name for this pipeline or wildcard
         self.name = name
         self.value = value
         return None
 
-
-class Node(User):
     
-
-    
+'''
 class Job(Task,Configuration,Node):
     def __init__(self,task='',config='',node='',
                  state='',pid=1,starttime=1,endtime=2,
@@ -238,3 +245,5 @@ class Event(self,job='',name='',value='',options=''):
 
 
     
+class Node(User):
+''' 
