4100d0e839efc6903e03b309062df191ee9d5d0a
Rubab Khan <rubab@uw.edu>
2018-06-08 13:16:10 -0700

    Updated wpipe.py

--- wings_pipe/wpipe.py
+++ wings_pipe/wpipe.py
@@ -1,5 +1,5 @@
-#! /usr/bin/env python
-import time
+#! /usr/bin/env
+import time, subprocess, shlex
 import numpy as np
 import pandas as pd
 pd.set_option('io.hdf.default_format','table')
@@ -35,16 +35,19 @@ class Store():
                  'jobs': Job().new(),
                  'masks':  Mask().new(),
                  'events': Event().new()}
-        with pd.HDFStore(str(self.path),'w') as myStore:
+        with pd.HDFStore(str(self.path),'w',complevel=9,
+                    complib='blosc:blosclz') as myStore:
             for k,v in _dict.items():
-                myStore.append(k,v,min_itemsize=_min_itemsize(v))
+                myStore.append(k,v,min_itemsize=_min_itemsize(v),
+                          complevel=9,complib='blosc:blosclz')
         return None
 
     def create(self,key,name_id,stuff):
         with pd.HDFStore(str(self.path),'r+') as myStore:
             stuff.__dict__[name_id][0] = int(myStore[key][name_id].max())+1
             newStuff = stuff.new()
-            myStore.append(key,newStuff,min_itemsize=_min_itemsize(newStuff))
+            myStore.append(key,newStuff,min_itemsize=_min_itemsize(newStuff),
+                          complevel=9,complib='blosc:blosclz')
         return newStuff
         
     
@@ -54,6 +57,17 @@ class Store():
                 return myStore.select(str(key),columns=columns)
             else:
                 return myStore.select(str(key),str(where),columns=columns)
+            
+    def repack(self):
+        filename = str(self.path)
+        _t1 = ['cp', filename, './data/backup1.h5']
+        _t2 = ['ptrepack', '--chunkshape=auto', '--propindexes', '--complevel=9',
+               '--complib=blosc', filename, './data/temp1.h5']
+        _t3 = ['mv', './data/temp1.h5', filename]
+        _t = subprocess.run(_t1, stdout=subprocess.PIPE)
+        _t = subprocess.run(_t2, stdout=subprocess.PIPE)
+        _t = subprocess.run(_t3, stdout=subprocess.PIPE)
+        return
                 
 
 class User():
@@ -95,8 +109,7 @@ class Options():
         self.__dict__ = opts
         return None
 
-    def new(self,owner='any                                                      '
-            ,owner_id=0):
+    def new(self,owner=str('any'+250*' '),owner_id=0):
         name = np.array(list(self.__dict__.keys()))
         value = np.array(list(self.__dict__.values()))
         _df = pd.DataFrame(data=np.array([name,value]).T,columns=
@@ -110,25 +123,10 @@ class Options():
     def create(self,owner='any',owner_id=0,store=Store()):
         _df = self.new(owner,owner_id)
         with pd.HDFStore(str(store.path),'r+') as myStore:
-            myStore.append('options',_df,min_itemsize=_min_itemsize(_df))
+            myStore.append('options',_df,min_itemsize=_min_itemsize(_df),
+                          complevel=9,complib='blosc:blosclz')
         return _df
 
-'''
-    #needs work
-    def add_or_update(opt1,opt2,store=Store()):
-        owner,owner_id = opt2.index[0]
-        dict1 = dict(zip(opt1.values[:,0],opt1.values[:,1]))
-        dict2 = dict(zip(opt2.values[:,0],opt2.values[:,1]))
-        for key in list(dict2.keys()):
-            dict1[key] = dict2[key]
-        return Options(dict1).create(owner,owner_id)
-        
-    # needs work
-    def get_opt(opt,owner='any',owner_id=0,store=Store()):
-        
-        _array = opt.sort_index(level=['owner','owner_id']).loc[str(owner),int(owner_id)].values
-        return dict(zip(_array[:,0],_array[:,1]))
-'''
          
 class Pipeline():
     def __init__(self,user=User().new(),name='any',software_root='',
@@ -292,15 +290,7 @@ class Parameters():
             myStore.append('parameters',_df,min_itemsize=_min_itemsize(_df))
         return _df
 
-'''    
-    # needs work
-    def get_params(cls,config=Configuration().new()):
-        _config_id = int(config.config_id)
-        _target_id = int(config.target_id)
-        _pipeline_id = int(config.pipeline_id)
-        _array = cls.loc[_pipeline_id,_target_id,_config_id].values
-        return dict(zip(_array[:,0],_array[:,1]))
-'''    
+
 
 class Task():
     def __init__(self,name='any',flags='',
