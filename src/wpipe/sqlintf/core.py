1269456a3ae617a310819394c452ca20a7c5b6ea
Tristan Hillis <tristan3214@live.com>
2020-05-31 17:14:07 -0600

    testing around with yaml file

--- src/wpipe/sqlintf/core.py
+++ src/wpipe/sqlintf/core.py
@@ -29,11 +29,13 @@ if sqlite:
 else:
     engine_URL = 'mysql://wpipe:W£|£3u53r@localhost/server'
 
-# try:
-#     engine = sa.create_engine(engine_URL)  # , echo=True)
-# except sa.exc.DatabaseError: # revert to some default on error
-#     engine = sa.create_engine("mysql+pymysql://root:password@localhost:8000/server")
-engine = sa.create_engine("mysql+pymysql://root:password@localhost:8000/server")
+engine = None
+try:
+    engine = sa.create_engine(engine_URL)  # , echo=True)
+except sa.exc.DatabaseError: # revert to some default on error
+    print("We got here")
+    engine = sa.create_engine("mysql+pymysql://root:password@localhost:8000/server")
+# engine = sa.create_engine("mysql+pymysql://root:password@localhost:8000/server")
 
 
 if not sqlite:
