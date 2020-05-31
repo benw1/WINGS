c00dc457dfeb6a7a97bdae83c67ed67b686ee518
Tristan Hillis <tristan3214@live.com>
2020-05-31 14:25:28 -0600

    add dependency

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(name='wpipe',
       version=__version__,
       packages=['wpipe', 'wpipe.sqlintf'],
       package_dir={'': 'src'},
-      install_requires=['numpy', 'pandas', 'tables', 'sqlalchemy', 'mysql-connector-python', 'astropy'],
+      install_requires=['numpy', 'pandas', 'tables', 'sqlalchemy',
+                        'mysql-connector-python', 'mysqlclient', 'astropy'],
       scripts=['bin/wingspipe']
       )
