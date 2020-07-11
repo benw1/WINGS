2022f679eab9ec53bafeccf504d7924efe4f2f74
Adrien Thob <adrien.thob@gmail.com>
2020-07-11 14:43:00 -0700

    Including jinja in install requirements

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name='wpipe',
       python_requires='>=3',
       packages=['wpipe', 'wpipe.sqlintf'],
       package_dir={'': 'src'},
-      install_requires=['numpy', 'pandas', 'tables', 'sqlalchemy',
+      install_requires=['numpy', 'pandas', 'tables', 'jinja', 'sqlalchemy',
                         'mysql-connector-python', 'mysqlclient', 'astropy'],
       scripts=['bin/wingspipe']
       )
