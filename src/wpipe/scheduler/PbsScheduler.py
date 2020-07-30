a406658a0ad92c792def91d5e229fbe8ec3c9d17
Benjamin Williams <bwilli24@wfe.head.p9.nas.nasa.gov>
2020-07-30 13:12:18 -0700

    improved execute and starter for key object in scheduler

--- src/wpipe/scheduler/PbsScheduler.py
+++ src/wpipe/scheduler/PbsScheduler.py
@@ -13,21 +13,21 @@ class PbsScheduler(BaseScheduler):
         self.key = self.task.name
         #self.key = PbsKey(schedulers,job)
         test_new = self.key.equals(self)
-        PbsScheduler.schedulers.append(self) # add this new scheduler to the list
-
+        if test_new == 0:
+           PbsScheduler.schedulers.append(self) # add this new scheduler to the list
+        if test_new > 0:
+           ##How do we add the job to the already-existing scheduler of this key?
         # run the submit now that the object is created
         self._submit()
-    ### Not sure we need this, since we can probaby define the key in the PbsScheduler object...
     class PbsKey:
         def __init__(self,schedulers,event)
-            self.key = job.task.name
-        return 
-    def equals(self):
-        check = 0
-        for key in self.schedulers:
-            if self.schedulers[key] == self.key
-               check = 1
-        return(check)
+            self.key = self.pipeline.pipeline_id+job.task.name
+        def equals(self):
+            check = 0
+            for key in self.schedulers:
+                if self.schedulers[key] == self.key
+                   check = 1
+            return(check)
  
             
     #######################
@@ -43,12 +43,20 @@ class PbsScheduler(BaseScheduler):
     def _execute(self):
         print("We do the scheduling now from: " + self.key)
         #Put pbs and list of tasks in the configuration root
+        pbsfilepath = setup_pbs(self)
+        qsub_command = "qsub "+pbsfilepath
+        pbs_id = os.popen(qsub_command) 
+        ##Need to keep track of the pbs job id.  Where can we put this?
+        # remove scheduler from list
+        PbsScheduler.schedulers.remove(self)
+
+    def setup_pbs(self):
         now = datetime.now()
         dt_string = now.strftime("%d-%m-%Y-%H-%M-%S.%f")
         pbsfilename = dt_string+".pbs #name it with the current time
         executables_file = dt_string+".list #name it with the current time
-        pbsfilepath = self.event.target.conf_root+pbsfilename
-        executabls_path = self.event.target.conf_root+executables_file
+        pbsfilepath = self.pipeline.config_root+"/"+pbsfilename
+        executabls_path = self.pipeline.config_root+"/"+executables_file
         njobs = len(self.jobs)
         pbsfile = open(pbsfilepath,"w")
         pbsfile.write("#PBS -S /bin/bash\n")
@@ -57,7 +65,7 @@ class PbsScheduler(BaseScheduler):
         pbsfile.write("#PBS -W group_list=s1692\n")
         pbsfile.write("#PBS -l walltime=24:00:00\n")
         pbsfile.write("source ~/.bashrc\n")
-        pbsfile.write("cd ",self.event.target.proc_root\n)
+        pbsfile.write("cd ",self.pipeline.pipe_root\n)
         pbsfile.write("parallel --jobs",str(njobs),"--sshloginfile $PBS_NODEFILE --workdir $PWD < ",executables_list_path,"\n")
         pbsfile.close()
         executables_list = open(executables_path,"w")
@@ -66,12 +74,7 @@ class PbsScheduler(BaseScheduler):
             jobid = job.job_id 
             executables_list.write(str(executable)," -j ",jobid)
         executables_list.close()
-        qsub_command = "qsub "+pbsfilepath
-        pbs_id = os.popen(qsub_command) 
-        ##Need to keep track of the pbs job id.  Where can we put this?
-        # remove scheduler from list
-        PbsScheduler.schedulers.remove(self)
-
+        return(pbsfilepath)        
 
     @staticmethod
     def _checkForScheduler(key):
