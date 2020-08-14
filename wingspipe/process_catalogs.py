80bb049bf362392f32548b42b476851d5adcfd4b
Benjamin Williams <bwilli24@wfe.head.p9.nas.nasa.gov>
2020-08-14 15:20:46 -0700

    preparing 18 detector splitting correctly

--- wingspipe/process_catalogs.py
+++ wingspipe/process_catalogs.py
@@ -433,7 +433,7 @@ if __name__ == '__main__':
                 for i in range(ndetect):
                     dpid = dp_id
                     new_event = my_job.child_event('split_catalog', tag=i+1,
-                                       options={'dp_id': dpid})
+                                       options={'dp_id': dpid,'submission_type':'pbs'})
                     my_job.logprint(''.join(["Firing event ", str(new_event.event_id), "  split_catalog"]))
                     new_event.fire()
         elif 'split' in event.name:
