af567bb6f5bdf4c3aa068b0c8b360abd84681000
Adrien Thob <adrien.thob@gmail.com>
2020-10-01 15:56:04 -0700

    Memory usage improvement in wingtips

--- wingspipe/process_catalogs.py
+++ wingspipe/process_catalogs.py
@@ -296,7 +296,7 @@ def getgalradec(infile, ra, dec, magni, background):
     filedir = background + '/'
     outfile = starpre + '_' + filt + '.tbl'
     flux = wtips.get_counts(magni[:, 0], zp_ab[0])
-    wtips.from_scratch(flux=flux, ra=ra, dec=dec, outfile=outfile)
+    wtips.from_scratch(flux=flux, ra=ra, dec=dec, outfile=outfile, max_writing_packet=int(np.round(len(flux)/100)))
     stars = wtips([outfile])
     galaxies = wtips([filedir + filt + '.txt'])  # this file will be provided pre-made
     radec = galaxies.random_radec_for(stars)
