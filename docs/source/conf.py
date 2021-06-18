2256066b09f957c59f0ae70f1b22c9c57ec539cb
benw1 <benw1@users.noreply.github.com>
2021-06-18 16:37:19 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.autosectionlabel']
-extensions = ['sphinx-autoapi', 'sphinx.ext.napoleon', 'sphinx.ext.autosectionlabel']
+extensions = ['autoapi.extension', 'sphinx.ext.napoleon', 'sphinx.ext.autosectionlabel']
 napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
