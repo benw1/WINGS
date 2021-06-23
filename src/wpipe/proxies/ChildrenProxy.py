c6e84233fa9264484d7e5cdf6db3a0b0d9d11875
Adrien Thob <adrien.thob@gmail.com>
2021-06-22 17:45:04 -0700

    Fixing deletion of DictLikeChildrenProxy with None proxies

--- src/wpipe/proxies/ChildrenProxy.py
+++ src/wpipe/proxies/ChildrenProxy.py
@@ -94,7 +94,7 @@ class ChildrenProxy:  # TODO: Generalize proxy object with the BaseProxy
     @in_session('_parent')
     def delete(self):
         while len(self):
-            self[0].delete()
+            self._search_child_from_attritem(0).delete()
 
     @contextlib.contextmanager
     @in_session('_parent', generator=True)
