bc35aa442af4c719d8d12e061640b72229fee385
Adrien Thob <adrien.thob@gmail.com>
2022-05-06 16:47:50 -0400

    Missing requirement for some cases

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(name='wpipe',
       python_requires='>=3',
       packages=['wpipe', 'wpipe.sqlintf', 'wpipe.proxies', 'wpipe.scheduler'],
       package_dir={'': 'src'},
-      install_requires=['numpy', 'pandas', 'tenacity', 'tables', 'sqlalchemy',
+      install_requires=['numpy', 'pandas', 'tenacity', 'tables', 'sqlalchemy', 'pymysql',
                         'mysql-connector-python', 'mysqlclient', 'astropy', 'jinja2'],
       scripts=['bin/wingspipe', 'bin/pbsconsumer.py']
       )
