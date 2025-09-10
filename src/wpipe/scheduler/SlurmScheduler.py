5255daf08cbfa69a8b9c0c97a835bec15cb65392
Tristan <tjhillis@uw.edu>
2025-09-10 15:21:51 +0000

    add some print statements

--- src/wpipe/scheduler/SlurmScheduler.py
+++ src/wpipe/scheduler/SlurmScheduler.py
@@ -5,6 +5,7 @@ Contains the scheduler.SlurmScheduler class definition
 Please note that this module is private. The scheduler.SlurmScheduler class is
 available in the ``wpipe.scheduler`` namespace - use that instead.
 """
+
 import datetime
 import math
 
@@ -12,15 +13,14 @@ from .BaseScheduler import BaseScheduler
 from .TemplateFactory import TemplateFactory
 import subprocess
 
-__all__ = ['DEFAULT_NODE_MODEL', 'DEFAULT_WALLTIME', 'SlurmScheduler']
+__all__ = ["DEFAULT_NODE_MODEL", "DEFAULT_WALLTIME", "SlurmScheduler"]
 
-DEFAULT_WALLTIME = '48:00:00'
-DEFAULT_MEMORY = '50G'
-DEFAULT_ACCOUNT = 'astro'
-DEFAULT_PARTITION = 'astro'
-DEFAULT_NODE_MODEL = 'has'
-NODE_CORES_DICT = {'bro': 2 * 14, 'has': 2 * 12, 'ivy': 2 * 10, 'san': 2 * 8}
-
+DEFAULT_WALLTIME = "48:00:00"
+DEFAULT_MEMORY = "50G"
+DEFAULT_ACCOUNT = "astro"
+DEFAULT_PARTITION = "astro"
+DEFAULT_NODE_MODEL = "has"
+NODE_CORES_DICT = {"bro": 2 * 14, "has": 2 * 12, "ivy": 2 * 10, "san": 2 * 8}
 
 
 class SlurmScheduler(BaseScheduler):
@@ -28,9 +28,9 @@ class SlurmScheduler(BaseScheduler):
     schedulers = list()
 
     def __init__(self, jobdata):
-
         super().__init__(
-            jobdata.getTime() if jobdata.getTime() is not None else 20)  # passed in value or default timer amount (seconds).
+            jobdata.getTime() if jobdata.getTime() is not None else 20
+        )  # passed in value or default timer amount (seconds).
         print("Creating a new scheduler ...")
 
         self._key = self.SlurmKey(jobdata)
@@ -51,8 +51,8 @@ class SlurmScheduler(BaseScheduler):
         self._jobList.append(jobdata)
 
         # Reset the scheduler
-       
-        if (len(self._jobList) > listmax):
+
+        if len(self._jobList) > listmax:
             super().run_it()
         else:
             super().reset()
@@ -67,18 +67,22 @@ class SlurmScheduler(BaseScheduler):
         executables_file = dt_string + ".list"  # name it with the current time
 
         slurmfilepath = self._jobList[0].getPipelineConfigRoot() + "/" + slurmfilename
-        executables_path = self._jobList[0].getPipelineConfigRoot() + "/" + executables_file
+        executables_path = (
+            self._jobList[0].getPipelineConfigRoot() + "/" + executables_file
+        )
 
         jobFileOutput = self._makeJobList()
         slurmFileOutput = self._makeSlurmFile(executables_path)
 
-        with open(executables_path, 'w') as f:
+        with open(executables_path, "w") as f:
             f.write(jobFileOutput)
-        with open(slurmfilepath, 'w') as f:
+        with open(slurmfilepath, "w") as f:
             f.write(slurmFileOutput)
 
         # TODO: Test this out more
-        output = subprocess.run("sbatch %s" % (slurmfilepath), shell=True, capture_output=True)
+        output = subprocess.run(
+            "sbatch %s" % (slurmfilepath), shell=True, capture_output=True
+        )
         print("Sbatch output:")
         print(output)
 
@@ -107,13 +111,21 @@ class SlurmScheduler(BaseScheduler):
         n_cpus = node_cores[node_model]
         for jobdata in self._jobList:
             jobsForJinja.append(
-                {'command': ("export OMP_NUM_THREADS=%d && " % n_cpus if omp_threads else "")
-                            + "source ~/.bashrc && conda activate %s &&" % jobdata.getCondaEnv()
-                            + jobdata.getTaskExecutable()
-                            + ' -p ' + str(jobdata.getPipelineId())
-                            + ' -u ' + str(jobdata.getPipelineUserName())
-                            + ' -j ' + str(jobdata.getJobId())
-                            + bool(jobdata.getVerbose()) * ' -v'})
+                {
+                    "command": (
+                        "export OMP_NUM_THREADS=%d && " % n_cpus if omp_threads else ""
+                    )
+                    + "source ~/.bashrc && conda activate %s &&" % jobdata.getCondaEnv()
+                    + jobdata.getTaskExecutable()
+                    + " -p "
+                    + str(jobdata.getPipelineId())
+                    + " -u "
+                    + str(jobdata.getPipelineUserName())
+                    + " -j "
+                    + str(jobdata.getJobId())
+                    + bool(jobdata.getVerbose()) * " -v"
+                }
+            )
 
         output = template.render(jobs=jobsForJinja)
         print()
@@ -123,7 +135,6 @@ class SlurmScheduler(BaseScheduler):
         return output
 
     def _makeSlurmFile(self, executablesListPath):
-
         template = TemplateFactory.getSlurmFileTemplate()
 
         slurm_account = self._jobList[0].getAccount()
@@ -135,18 +146,20 @@ class SlurmScheduler(BaseScheduler):
         n_nodes = [math.ceil(n_jobs / node_cores[node_model]), n_jobs][omp_threads]
         n_cpus = node_cores[node_model]
         n_jobs_per_node = [n_cpus, 1][omp_threads]
-        omp_threads = ['', 'ompthreads=%d:' % n_cpus][omp_threads]
+        omp_threads = ["", "ompthreads=%d:" % n_cpus][omp_threads]
 
         # create a dictionary
-        slurmDict = {'nnodes': n_nodes,
-                   'njobs': n_jobs_per_node,
-                   'walltime': self._jobList[0].getWalltime(),
-                   'mem' : self._jobList[0].getMemory(),
-                   'account' : self._jobList[0].getAccount(),
-                   'partition' : self._jobList[0].getPartition(),
-                   'jobid' : self._jobList[0].getJobId(),
-                   'pipe_root': self._jobList[0].getPipelinePipeRoot(),
-                   'executables_list_path': executablesListPath}
+        slurmDict = {
+            "nnodes": n_nodes,
+            "njobs": n_jobs_per_node,
+            "walltime": self._jobList[0].getWalltime(),
+            "mem": self._jobList[0].getMemory(),
+            "account": self._jobList[0].getAccount(),
+            "partition": self._jobList[0].getPartition(),
+            "jobid": self._jobList[0].getJobId(),
+            "pipe_root": self._jobList[0].getPipelinePipeRoot(),
+            "executables_list_path": executablesListPath,
+        }
 
         output = template.render(slurm=slurmDict)
 
@@ -168,7 +181,11 @@ class SlurmScheduler(BaseScheduler):
 
         (hasScheduler, scheduler) = SlurmScheduler._checkForScheduler(jobdata)
         if hasScheduler:  # check for existing schedulers and call submitJob for the retrieved scheduler
-            print('Adding job to scheduler with key {} ...'.format(scheduler._key.getKey()))
+            print(
+                "Adding job to scheduler with key {} ...".format(
+                    scheduler._key.getKey()
+                )
+            )
             scheduler._submitJob(jobdata)
         else:  # No scheduler was found but we need to do the scheduling
             SlurmScheduler(jobdata)
@@ -179,11 +196,15 @@ class SlurmScheduler(BaseScheduler):
 
     # out of site and out of mind
     class SlurmKey(object):
-
         def __init__(self, jobdata):
             # self._key = jobdata.getTaskName()  # For debugging
-            self._key = str(jobdata.getPipelineId()) + jobdata.getTaskName() + jobdata.getNodemodel() +\
-                        jobdata.getWalltime() + ['', 'OpenMP'][jobdata.getJobOpenMP()]
+            self._key = (
+                str(jobdata.getPipelineId())
+                + jobdata.getTaskName()
+                + jobdata.getNodemodel()
+                + jobdata.getWalltime()
+                + ["", "OpenMP"][jobdata.getJobOpenMP()]
+            )
 
         def equals(self, other):
             if self._key == other.getKey():
